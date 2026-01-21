bf975893cb039b78ec3f7aab020a7221faf9f2f3
deco-L <scotchwitchbroom@gmail.com>
2026-01-21 22:17:43 +0900

    ♻ refactor .gitignore & change frontend directory structure

--- srcs/backend/src/index.js
+++ srcs/backend/src/index.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/healthz", (_req, res) => {
-  res.status(200).send("ok");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`backend listening on port ${PORT}`);
